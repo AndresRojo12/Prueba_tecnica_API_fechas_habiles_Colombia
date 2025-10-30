b90ad408f5a50f41e1006e40d5dab51ae9bb99c7
AndresRojo12 <andresrojo20013@gmail.com>
2025-10-30 18:29:07 -0500

    Se actualiza el index principal, donde se levanta el servidor

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,29 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
 
+import workingDaysRoutes from './routes/workingDays.routes.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+// Configurar __dirname
 
+// Cargar variables de entorno
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
+app.use(express.json());
 
+// Puerto desde .env
 const PORT = process.env.PORT || 3000;
 
+// Ruta de prueba
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+// Rutas del módulo de días hábiles
+app.use('/', workingDaysRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`✅ Server is running on port ${PORT}`);
 });
