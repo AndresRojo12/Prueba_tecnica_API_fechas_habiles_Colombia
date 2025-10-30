de222b2f243c91d07fad562fa31bf543a8f38fbd
AndresRojo12 <andresrojo20013@gmail.com>
2025-10-30 18:25:58 -0500

    Se crea la ruta personalizada

--- src/routes/workingDays.routes.ts
+++ src/routes/workingDays.routes.ts
@@ -1,7 +1,7 @@
 import {  Router  } from 'express';
-
+import { calculateWorkingDate } from '../controllers/workingDays.controller';
 const router = Router();
 
-router.get('/calculate-date')
+router.get('/calculate-date', calculateWorkingDate)
 
 export default router;
