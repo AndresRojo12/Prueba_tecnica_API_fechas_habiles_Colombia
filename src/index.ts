00dce09118e60633e1c9a39145bf4ad53070f93a
AndresRojo12 <andresrojo20013@gmail.com>
2025-11-03 19:36:03 -0500

    Se importa el middleware para validar errores

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
+import express from "express";
+import * as dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 
-import workingDaysRoutes from './routes/workingDays.routes.js';
+import {
+  logError,
+  boomErrorHandler,
+  genericErrorHandler,
+} from "./utils/middleware/errorHandler.js";
+import workingDaysRoutes from "./routes/workingDays.routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // Configurar __dirname
 
 // Cargar variables de entorno
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
 app.use(express.json());
@@ -25,7 +30,12 @@ app.get("/", (req, res) => {
 });
 
 // Rutas del módulo de días hábiles
-app.use('/', workingDaysRoutes);
+app.use("/", workingDaysRoutes);
+
+// manejadores de errores (middlewares)
+app.use(logError);
+app.use(boomErrorHandler);
+app.use(genericErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
