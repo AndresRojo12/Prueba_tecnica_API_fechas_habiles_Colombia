fcb4346a1666305774a7baeba317ed1cf549b780
AndresRojo12 <andresrojo20013@gmail.com>
2025-11-03 19:32:16 -0500

    Se pasa el middleware validador a las rutas

--- src/routes/workingDays.routes.ts
+++ src/routes/workingDays.routes.ts
@@ -1,8 +1,9 @@
 //Manejador de ruta personalisada para calcular fechas laborales
-import {  Router  } from 'express';
-import { calculateWorkingDate } from '../controllers/workingDays.controller.js';
+import { Router } from "express";
+import { calculateWorkingDate } from "../controllers/workingDays.controller.js";
+import { validateWorkingDays } from "../utils/middleware/handleValidateDate.js";
 const router = Router();
 
-router.get('/calculate-date', calculateWorkingDate)
+router.get("/calculate-date", validateWorkingDays, calculateWorkingDate);
 
 export default router;
